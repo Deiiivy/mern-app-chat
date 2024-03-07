b66a56848dea056e81013a83876aba6dd746286d
Deiiivy <deivypr28@gmail.com>
2024-03-06 19:23:55 -0500

    config basic backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const { Socket } = require('socket.io')
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server,  {
+    cors: { origin: '*' }
+})
+
+io.on('conneection', (Socket) => {
+    console.log('one client coneected');
+
+    Socket.on('message', (data) => {
+        io.emit('messages', data)
+        console.log(data);
+    })
+})
+
+
+app.get('/', (req,res) => {
+    res.send('Hola mundo, esto es un chat')
+})
+server.listen(4000,() => {
+    console.log('server run in the port localhost:4000');
+})
